74de092a002203623ebb3374bb6c244c2543d842
Josemargal <126793937+Josemargal@users.noreply.github.com>
2025-05-19 21:09:58 +0200

    subida catalog

---
+++ src/main/java/com/example/demo/StudentCatalogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentCatalogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentCatalogServiceApplication.class, args);
+	}
+
+}
